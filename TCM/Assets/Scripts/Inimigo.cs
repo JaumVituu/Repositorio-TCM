b83388efa247ecec64e9ec24c007da26d661772e
Leonardo-Silva-Oliveira <leonardo.oliveira611@etec.sp.gov.br>
2021-12-10 10:42:46 -0300

    Criação de script de outros inimigos

--- TCM/Assets/Scripts/Inimigo.cs
+++ TCM/Assets/Scripts/Inimigo.cs
@@ -9,38 +9,92 @@ public class Inimigo : MonoBehaviour
     [SerializeField]
     Transform jogador;
 
-    [SerializeField]
-    GameObject municao;
-
-    float campodevisao = 10.0f;
+    float campodevisao;
+    float velocidade = 3.0f;
+    Rigidbody2D rb2d;
 
     // Start is called before the first frame update
     void Start()
     {
         imagem = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float distancia = Vector2.Distance(transform.position, jogador.position);
-
-        if (distancia < campodevisao)
+        if (gameObject.tag == "Inimigo")
         {
-            if (transform.position.x < jogador.position.x)
+            campodevisao = 10.0f;
+            if (distancia < campodevisao)
             {
-                transform.localScale = new Vector2(4, 4);
-                imagem.Play("soldado_atento");
+                if (transform.position.x < jogador.position.x)
+                {
+                    transform.localScale = new Vector2(4, 4);
+                    imagem.Play("soldado_atento");
+                }
+                else
+                {
+                    transform.localScale = new Vector2(-4, 4);
+                    imagem.Play("soldado_atento");
+                }
             }
             else
             {
-                transform.localScale = new Vector2(-4, 4);
-                imagem.Play("soldado_atento");
+                imagem.Play("soldado_parado");
             }
         }
-        else
+        if (gameObject.tag == "Cachorro")
         {
-            imagem.Play("soldado_parado");
+            campodevisao = 6.0f;
+            if (distancia < campodevisao)
+            {
+                if (transform.position.x < jogador.position.x)
+                {
+                    rb2d.velocity = new Vector2(velocidade, 0);
+                    transform.localScale = new Vector2(-3, 3);
+                    imagem.Play("Cachorro_andando");
+                }
+                else
+                {
+                    rb2d.velocity = new Vector2(-velocidade, 0);
+                    transform.localScale = new Vector2(3, 3);
+                    imagem.Play("Cachorro_andando");
+                }
+            }
+            else
+            {
+                rb2d.velocity = new Vector2(0, 0);
+                imagem.Play("Cachorro_parado");
+            }
+        }
+        if (gameObject.tag == "Chefe")
+        {
+            campodevisao = 10.0f;
+            if (distancia < campodevisao)
+            {
+
+                if (transform.position.x < jogador.position.x)
+                {
+
+                    rb2d.velocity = new Vector2(velocidade, 0);
+                    transform.localScale = new Vector2(0.4f, 0.4f);
+                    imagem.Play("Veio_andando");
+                }
+                else
+                {
+
+                    rb2d.velocity = new Vector2(-velocidade, 0);
+                    transform.localScale = new Vector2(-0.4f, 0.4f);
+                    imagem.Play("Veio_andando");
+                }
+
+            }
+            else {
+                rb2d.velocity = new Vector2(0, 0);
+                imagem.Play("Veio_parado");
+            }
         }
     }
 }
